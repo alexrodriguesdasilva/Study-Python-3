a329d95af622d1f262ceccd00380f4fcbfa9cf6c
Alex Rodrigues da Silva <alerodrigues@uolinc.com>
2023-02-11 17:10:35 -0300

    adicionando aula 008

---
+++ curso-em-video/Aulas/aula-008/exemplo03.py
@@ -0,0 +1,5 @@
+from math import sqrt, floor
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print(f'A raiz de {num} é {raiz}')
+print(f'A raiz de {num} arredondada para baixo é {floor(raiz)}')
